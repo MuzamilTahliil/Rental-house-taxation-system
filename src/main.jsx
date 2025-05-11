a08c778e4fb6903b46e9db9b4d84d083dba87ba6
MuzamilTahliil <muzamiltahliil1@gmail.com>
2025-05-11 13:15:07 +0300

    updated files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App'; // Your main app
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter> {/* Only include this once here */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
