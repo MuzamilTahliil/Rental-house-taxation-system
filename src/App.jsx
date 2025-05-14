2357521af89905e4ddb723a0d96665ef9e2bd016
ayaandhaqane <148534224+ayaandhaqane@users.noreply.github.com>
2025-05-14 15:41:48 +0300

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import AdminSignup from './pages/adminSignup'
-import SignInPage from './pages/adminSignup'
-import AdminSignIn from './pages/adminSignin'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminDashboard from './pages/AdminDashboard';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
+import MessageForm from './components/MessageForm';
+import Report from './pages/Report';
+import RentalTransactions from './pages/Transaction';
+import Prediction from './pages/Prediction';
+import DistrictTable from './pages/District';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-   
-      <Routes>
-      <Route path="/" element={<AdminSignup/>} /> 
-      <Route path="/signin" element={<AdminSignIn/>} /> 
+    <Router>
+      <div className="flex">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main content area */}
+        <div className="flex-1">
+          {/* Navbar */}
+          <Navbar />
+          {/* Define the Routes for your app */}
+          <Routes>
+            {/* Main Dashboard route */}
+            <Route path="/" element={<AdminDashboard />} />
+            <Route path="/messages" element={<MessageForm />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="/transactions" element={<RentalTransactions />} />
+            <Route path="/prediction" element={<Prediction />} />
+            <Route path="/districts" element={<DistrictTable />} />
 
 
-    </Routes>
-    
-  )
-}
 
-export default App
+
+
+            {/* Add other routes here as needed */}
+            {/* Example:
+            <Route path="/users" element={<Users />} />
+            */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
